700355bf608f13c7033b898ea88dabf33edd966c
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2020-01-21 15:39:03 +0530

    Add files via upload

---
+++ spring-boot-security/src/main/java/com/roytuts/spring/boot/security/SpringBootAngularSecurityApp.java
@@ -0,0 +1,12 @@
+package com.roytuts.spring.boot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularSecurityApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularSecurityApp.class, args);
+	}
+}
---
+++ spring-boot-security/src/main/java/com/roytuts/spring/boot/security/controller/UIRestController.java
@@ -0,0 +1,29 @@
+package com.roytuts.spring.boot.security.controller;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UIRestController {
+
+	@GetMapping("/auth")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	@GetMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+
+}
