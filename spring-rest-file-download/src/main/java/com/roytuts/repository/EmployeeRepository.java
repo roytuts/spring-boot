bb3640d0b2ecede06d0bb8239fe8c4e6aaf4efa8
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2020-03-16 15:11:52 +0530

    Delete EmployeeRepository.java

--- spring-rest-file-download/src/main/java/com/roytuts/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.roytuts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.roytuts.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
