b2757ee4ce9131389e7fbbdd294687edaa298047
Soumitra <email@email.com>
2021-04-05 08:06:34 +0530

    spring boot whitelabel error page

---
+++ spring-boot-whitelabel-error-page/readme.rst
@@ -0,0 +1,1 @@
+You can read tutorial https://roytuts.com/how-to-deal-with-whitelabel-error-page-in-spring-boot-applications/
---
+++ spring-boot-whitelabel-error-page/src/main/java/com/roytuts/spring/boot/whitelabel/error/page/SpringBootWhiteLabelErrorPageApp.java
@@ -0,0 +1,15 @@
+package com.roytuts.spring.boot.whitelabel.error.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@SpringBootApplication
+public class SpringBootWhiteLabelErrorPageApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWhiteLabelErrorPageApp.class, args);
+	}
+
+}
