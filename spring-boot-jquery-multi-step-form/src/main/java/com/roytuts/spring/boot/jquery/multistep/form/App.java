9485a6247c96e956dd9a2112f41b8455aae16e83
Soumitra <projwebinfo@gmail.com>
2022-05-12 12:05:22 +0530

    Create App.java

---
+++ spring-boot-jquery-multi-step-form/src/main/java/com/roytuts/spring/boot/jquery/multistep/form/App.java
@@ -0,0 +1,15 @@
+package com.roytuts.spring.boot.jquery.multistep.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EntityScan(basePackages = "com.roytuts.spring.boot.jquery.multistep.form")
+//@EnableJpaRepositories(basePackages = "com.roytuts.spring.boot.jquery.multistep.form")
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
