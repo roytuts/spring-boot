5b2bcf946b842c079912442821ea86e395252bd0
Soumitra <projwebinfo@gmail.com>
2022-05-12 12:06:27 +0530

    Create UserRepository.java

---
+++ spring-boot-jquery-multi-step-form/src/main/java/com/roytuts/spring/boot/jquery/multistep/form/UserRepository.java
@@ -0,0 +1,7 @@
+package com.roytuts.spring.boot.jquery.multistep.form;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
