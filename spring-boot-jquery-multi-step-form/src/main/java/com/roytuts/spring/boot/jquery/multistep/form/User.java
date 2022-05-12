dea050060e74b42c765544569abacddcb97458bd
Soumitra <projwebinfo@gmail.com>
2022-05-12 12:06:06 +0530

    Create User.java

---
+++ spring-boot-jquery-multi-step-form/src/main/java/com/roytuts/spring/boot/jquery/multistep/form/User.java
@@ -0,0 +1,119 @@
+package com.roytuts.spring.boot.jquery.multistep.form;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotEmpty
+	@Column
+	private String name;
+
+	@Email
+	@NotBlank
+	@Column
+	private String email;
+
+	@NotNull
+	@Column
+	private String password;
+
+	@Min(10)
+	@Column
+	private Long phone;
+
+	@NotEmpty
+	@Column
+	private String gender;
+
+	@NotEmpty
+	@Column
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private String dob;
+
+	@NotNull
+	@Column
+	private String address;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
