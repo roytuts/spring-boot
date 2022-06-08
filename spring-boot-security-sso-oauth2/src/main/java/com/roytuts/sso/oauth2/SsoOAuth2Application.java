00d3ef5957e1737abb802b1762a4ba18039b455c
Soumitra <projwebinfo@gmail.com>
2022-06-08 16:19:29 +0530

    Create SsoOAuth2Application.java

---
+++ spring-boot-security-sso-oauth2/src/main/java/com/roytuts/sso/oauth2/SsoOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.roytuts.sso.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoOAuth2Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SsoOAuth2Application.class, args);
+	}
+	
+}
