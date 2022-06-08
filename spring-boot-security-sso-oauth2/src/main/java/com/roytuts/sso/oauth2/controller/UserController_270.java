1bf538cf010223de5de52f20a08f762363a06906
Soumitra <projwebinfo@gmail.com>
2022-06-08 16:19:02 +0530

    Create UserController_270.java

---
+++ spring-boot-security-sso-oauth2/src/main/java/com/roytuts/sso/oauth2/controller/UserController_270.java
@@ -0,0 +1,18 @@
+package com.roytuts.sso.oauth2.controller;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+}
