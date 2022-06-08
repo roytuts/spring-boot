3904cf53e418476dd229d9b78aff9b23009b0580
Soumitra <projwebinfo@gmail.com>
2022-06-08 16:18:13 +0530

    Update SsoOAuth2Application.java

--- spring-boot-security-sso-oauth2/src/main/java/com/roytuts/sso/oauth2/main/SsoOAuth2Application.java
+++ spring-boot-security-sso-oauth2/src/main/java/com/roytuts/sso/oauth2/main/SsoOAuth2Application.java
@@ -1,13 +1,13 @@
-package com.roytuts.sso.oauth2.main;
+package com.roytuts.sso.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.roytuts.sso.oauth2")
+@SpringBootApplication
 public class SsoOAuth2Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SsoOAuth2Application.class, args);
 	}
-
+	
 }
