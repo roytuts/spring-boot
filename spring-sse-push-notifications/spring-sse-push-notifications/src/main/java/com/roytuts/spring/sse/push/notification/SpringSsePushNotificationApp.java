b941732db45fd29e16295e014f4783a760179ce5
Soumitra <projwebinfo@gmail.com>
2023-07-19 17:33:52 +0530

    Update SpringSsePushNotificationApp.java

--- spring-sse-push-notifications/spring-sse-push-notifications/src/main/java/com/roytuts/spring/sse/push/notification/SpringSsePushNotificationApp.java
+++ spring-sse-push-notifications/spring-sse-push-notifications/src/main/java/com/roytuts/spring/sse/push/notification/SpringSsePushNotificationApp.java
@@ -3,7 +3,7 @@ package com.roytuts.spring.sse.push.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.roytuts.spring.sse.push.notification")
+@SpringBootApplication
 public class SpringSsePushNotificationApp {
 
 	public static void main(String[] args) {
