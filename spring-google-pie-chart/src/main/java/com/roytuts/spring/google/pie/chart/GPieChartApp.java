2f7502ff8024c5cf8682697e448dc9f4fa6619aa
Soumitra <projwebinfo@gmail.com>
2023-09-13 17:41:16 +0530

    Create GPieChartApp.java

---
+++ spring-google-pie-chart/src/main/java/com/roytuts/spring/google/pie/chart/GPieChartApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.google.pie.chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPieChartApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GPieChartApp.class, args);
+	}
+
+}
